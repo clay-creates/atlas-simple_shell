ec7b256b8ade844f5af20a25612ebacbbaaaeca4
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-07-22 20:23:48 +0000

    comments for prompt

--- prompt.c
+++ prompt.c
@@ -6,6 +6,10 @@
 
 void prompt(void)
 {
+	/**
+	 * Prompt method uses the "isatty" with STDIN_FILENO to check if the console is using the default input file,
+	 * If TRUE it prints the user prompt
+	 */
 	if (isatty(STDIN_FILENO))
 	{
 		printf("($) ");
