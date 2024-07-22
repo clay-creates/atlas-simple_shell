f13ea9594c58fed3cb4661386e2fa9033cb22312
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-07-22 20:38:09 +0000

    documentation and memory edits

--- shell.c
+++ shell.c
@@ -6,6 +6,24 @@
 
 void shell_loop(void)
 {
+	/**
+	 * Params:
+	 * *line: A character pointer to hold the user entry line after using read_ling method
+	 * **args: A character pointer to hold the tokenized line after using split_line method
+	 * status: Holds a default status code for error handling
+	 *
+	 * A Do / While loop is created to be the main loop of the console
+	 * Methods are called incrementally down the loop:
+	 * 1. The user is prompted for input.
+	 * 2. Store the user input line in the line var using read_line.
+	 * 3. If the input is null, check if the input is from a terminal, print a newline, and exit.
+	 * 4. The input line is tokenized and stored in the args array using split_line.
+	 * 5. If the first token is null, free the allocated memory for line and args and continue the loop.
+	 * 6. If the command is "exit", handle the exit process.
+	 * 7. If the command is "env", handle displaying the environment variables.
+	 * 8. For any other command, execute it and store the status.
+	 * 9. Free the allocated memory for line and args before the next iteration.
+	 */
 	char *line;
 	char **args;
 	int status = -1;
@@ -31,18 +49,21 @@ void shell_loop(void)
 		}
 		if (_strcmp(args[0], "exit") == 0)
 		{
+			free(line);
+			free(args);
 			handle_exit(args);
 		}
 		else if (_strcmp(args[0], "env") == 0)
 		{
 			handle_env(args);
+			free(line);
+			free(args);
 		}
 		else
 		{
 			status = execute(args);
+			free(line);
+			free(args);
 		}
-
-		free(line);
-		free(args);
 	} while (status);
 }
