a19673e327c0ebb7f39db1fe8ef1a0439fda50bd
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-07-23 14:58:18 +0000

    read_line documentation

--- read_line.c
+++ read_line.c
@@ -8,13 +8,13 @@
 
 char *read_line(void)
 {
-	char *line = NULL;
-	size_t bufsize = 0;
+	char *line = NULL;	/* Empty var to hold user input and return */
+	size_t bufsize = 0; /* Adjustable var to set size of memory for user input */
 
-	if (getline(&line, &bufsize, stdin) == -1)
+	if (getline(&line, &bufsize, stdin) == -1) /* If there is an error, free memory and return NULL */
 	{
 		free(line);
 		return (NULL);
 	}
-	return (line);
+	return (line); /* Return user input */
 }
