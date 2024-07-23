c6c5364f05abaf725fe4a5b862d7a900ad11e17b
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-07-23 15:41:36 +0000

    utils and builtins documentation

--- builtins.c
+++ builtins.c
@@ -4,9 +4,10 @@
  * handle_exit - Handles the exit command
  * @args: The arguments for the command
  */
+
 void handle_exit(char **args)
 {
-	if (args)
+	if (args) /* Ensure memory freed on exit */
 	{
 		free(args);
 		args = NULL;
@@ -18,16 +19,17 @@ void handle_exit(char **args)
  * handle_env - Handles the env command
  * @args: The arguments for the command
  */
+
 void handle_env(char **args)
 {
-	int i;
+	int i; /* Iteration var */
 
-	for (i = 0; environ[i] != NULL; i++)
+	for (i = 0; environ[i] != NULL; i++) /* For each env in environ print environ */
 	{
 		printf("%s\n", environ[i]);
 	}
 
-	if (args)
+	if (args) /* Ensure args memory is freed */
 	{
 		free(args);
 		args = NULL;
--- utils.c
+++ utils.c
@@ -7,12 +7,13 @@
  *
  * Return: 0 if strings are equal, non-zero otherwise
  */
+
 int _strcmp(const char *s1, const char *s2)
 {
-	while (*s1 && (*s1 == *s2))
+	while (*s1 && (*s1 == *s2)) /* While characters match continue comparing */
 	{
 		s1++;
 		s2++;
 	}
-	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+	return (*(unsigned char *)s1 - *(unsigned char *)s2); /* Return matching word */
 }
