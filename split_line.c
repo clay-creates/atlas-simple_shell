d6517ff8c3e1e3caedf6c37a139f5a5d48792225
clay-creates <122416165+clay-creates@users.noreply.github.com>
2024-07-23 15:14:35 +0000

    split_line documentation

--- split_line.c
+++ split_line.c
@@ -9,37 +9,46 @@
 
 char **split_line(char *line)
 {
+	/*
+	 * Params:
+	 * bufsize: Sets a standard size to hold tokens
+	 * position: An iteration var to move through the tokens array
+	 * tokens: Reserves memory for tokenized user input
+	 * token: Variable to hold individual tokens
+	 */
+
 	int bufsize = 64;
 	int position = 0;
 	char **tokens = malloc(bufsize * sizeof(char *));
 	char *token;
 
-	if (!tokens)
+	if (!tokens) /* Null check for tokens memory allocation */
 	{
-		fprintf(stderr, "Allocation Error\n");
-		free(line);
-		exit(EXIT_FAILURE);
+		fprintf(stderr, "Allocation Error\n"); /* Print error */
+		free(line);							   /* Free memory */
+		exit(EXIT_FAILURE);					   /* Exit with error */
 	}
 
-	token = strtok(line, " \t\r\n\a");
-	while (token != NULL)
-	{
-		tokens[position] = token;
-		position++;
+	token = strtok(line, " \t\r\n\a"); /* Separate user input into tokens, set delimiters */
 
-		if (position >= bufsize)
+	while (token != NULL) /* Start iterative action to fill tokens array */
+	{
+		tokens[position] = token; /* Current position of tokens array is assigned current token */
+		position++;				  /* Increment */
+
+		if (position >= bufsize) /* If tokens position exceeds set bufsize, increase bufsize */
 		{
-			bufsize += 64;
-			tokens = realloc(tokens, bufsize * sizeof(char *));
-			if (!tokens)
+			bufsize += 64;										/* Add to bufsize var */
+			tokens = realloc(tokens, bufsize * sizeof(char *)); /* Reallocate memory */
+			if (!tokens)										/* Null check after reallocation */
 			{
-				fprintf(stderr, "Allocation Error");
-				free(line);
-				exit(EXIT_FAILURE);
+				fprintf(stderr, "Allocation Error"); /* Print error */
+				free(line);							 /* Free memory */
+				exit(EXIT_FAILURE);					 /* Exit with error */
 			}
 		}
-		token = strtok(NULL, " \t\r\n\a");
+		token = strtok(NULL, " \t\r\n\a"); /* Set subsequent calls to strtok */
 	}
-	tokens[position] = NULL;
-	return (tokens);
+	tokens[position] = NULL; /* Null data at the end of the tokens array */
+	return (tokens);		 /* Return tokenized input */
 }
